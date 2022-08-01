0be93ae644e58331c85f2e21aa9f5436b9166892
MatheusAlmeida <matheus.ecomp@gmail.com>
2022-08-01 09:21:08 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import FileReader from './componnents/FileReader';
+import PageTitle from './componnents/PageTitle';
+
+function App() {
+  return (
+    <>
+      <PageTitle />
+      <FileReader />
+    </>
+    );
+}
+
+export default App;
---
+++ src/componnents/PageTitle.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function PageTitle() {
+  return (
+    <>
+        <h1>
+           Excel File Converter
+        </h1>
+    </>
+  )
+}
+
+export default PageTitle;
